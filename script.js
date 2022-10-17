ca4ffc0c6b367f8b33eb76447188384309e31649
Edgars Potkins <edgars.potkins@gmail.com>
2022-10-17 20:05:16 +0300

    Add basic calculator functionality

--- script.js
+++ script.js
@@ -12,6 +12,82 @@ function updateDisplay(){
     display.textContent = displayValue; 
 }
 
+updateDisplay();
+
+function clickButton() {
+    buttons.forEach((button) => {
+        button.addEventListener("click", function() {
+            if(button.classList.contains("operand")) {
+                inputOperand(button.value);
+                updateDisplay();
+            } else if(button.classList.contains("operator")) {
+                inputOperator(button.value);
+            } else if(button.classList.contains("equals")) {
+                inputEquals();
+                updateDisplay(); 
+            } else if(button.classList.contains("clear")) {
+                clearDisplay();
+                updateDisplay();
+            }
+        });
+    });
+}
+
+clickButton();
+
+function inputOperand(operand) {
+    if(firstOperator === null){
+        if(displayValue === "0" || displayValue === 0) {
+            displayValue = operand;
+        } else if (displayValue === firstOperand) {
+            displayValue = operand;
+        } else {
+            displayValue += operand;
+        }
+    } else {
+        if(displayValue === firstOperand) {
+            displayValue = operand;
+        } else {
+            displayValue +=operand;
+        }
+    }
+}
+
+function inputOperator(operator) {
+    if(firstOperator != null && secondOperator === null) {
+        secondOperator = operator;
+        secondOperand = displayValue;
+        result = operate(firstOperator, Number(firstOperand), Number(secondOperand));
+        displayValue = result.toString();
+        firstOperand = displayValue;
+        result = null;
+    } else {
+        firstOperator = operator;
+        firstOperand = displayValue;
+    }
+}
+
+function inputEquals() {
+    secondOperand = displayValue;
+    result = operate(firstOperator, Number(firstOperand), Number(secondOperand));
+    displayValue = result.toString();
+    firstOperand = displayValue;
+    secondOperand = null;
+    firstOperator = null;
+    secondOperator = null;
+    result = null;
+}
+
+
+function clearDisplay() {
+    displayValue = '0';
+    firstOperand = null;
+    secondOperand = null;
+    firstOperator = null;
+    secondOperator = null;
+    result = null;
+}
+
 function add(a, b){
     return a + b;
 }
